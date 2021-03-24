87d3b1a882d7d54710ad465ff9d461b73dba6341
bayu-17ajid <bayuajid12@gmail.com>
2021-03-24 20:13:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText emailid,pass;
+    Button daftar;
+    TextView masuk;
+    FirebaseAuth mfirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mfirebaseAuth = FirebaseAuth.getInstance();
+        emailid = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        daftar = findViewById(R.id.button);
+        masuk = findViewById(R.id.login);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String  email = emailid.getText().toString();
+                String pwd = pass.getText().toString();
+                if (email.isEmpty()){
+                    emailid.setText("Tolong masukkan eamil");
+                    emailid.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    pass.setText("Tolong masukkan password");
+                    pass.requestFocus();
+                }
+                else if(email.isEmpty() && pwd.isEmpty() ){
+                    Toast.makeText(MainActivity.this, "isi yang kosong", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && pwd.isEmpty())){
+                    mfirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Pendaftaran Gagal, coba lagi", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Terjadi kesalahan", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(a);
+            }
+        });
+    }
+}
